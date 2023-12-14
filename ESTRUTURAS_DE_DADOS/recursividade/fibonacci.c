93cdb7c470179c1c393fcbf400e0dba656a5f887
Mateus Cardoso <mat358591@gmail.com>
2023-12-14 09:20:09 -0300

    Update fibonacci.c

--- ESTRUTURAS_DE_DADOS/recursividade/fibonacci.c
+++ ESTRUTURAS_DE_DADOS/recursividade/fibonacci.c
@@ -9,11 +9,28 @@ int fib(int n,int v){
 	return fib(n-1,5)+fib(n-2,80);
 }
 
+void sequencia(int prim,int seg,int i,int n){
+	if(i==n) return;
+	
+	if(i<2){
+		printf("%d\t",prim);
+	}
+	else{
+		int aux=prim;
+		prim = seg;
+		seg = aux+prim;
+		printf("%d\t",seg);
+	}
+
+	sequencia(prim,seg,i+1,n);
+}
+
 int main(void){
 	//for(int i=0;i<7;i++){
-		printf("%d",fib(6,1));
+		printf("tt %d\n\n\n",fib(6,1));
 	//}
 	
+	sequencia(1,1,0,12);
 	
 	return 0;
 }
